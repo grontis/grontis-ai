86c0a876c70deb471108f423b508d440f21093b0
grontis <grantkveton@msn.com>
2026-06-20 01:07:09 -0600

    check_gpu and requirements from pip freeze

---
+++ check_gpu.py
@@ -0,0 +1,6 @@
+import torch
+
+print("PyTorch version:", torch.__version__)
+print("CUDA Available:", torch.cuda.is_available())
+if torch.cuda.is_available():
+    print("GPU:", torch.cuda.get_device_name())
